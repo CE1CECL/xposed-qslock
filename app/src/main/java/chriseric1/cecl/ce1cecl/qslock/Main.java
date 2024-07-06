98e307c3981097d29d7264494930483c66867ff5
Christopher Lentocha <christopherericlentocha@gmail.com>
2024-07-06 00:10:03 -0400

    Add CECL Changes

--- app/src/main/java/chriseric1/cecl/ce1cecl/qslock/Main.java
+++ app/src/main/java/chriseric1/cecl/ce1cecl/qslock/Main.java
@@ -1,4 +1,4 @@
-package com.github.char101.qslock;
+package chriseric1.cecl.ce1cecl.qslock;
 
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -6,57 +6,50 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import android.app.AndroidAppHelper;
-
 import android.content.Context;
 import android.util.Log;
 
 public class Main implements IXposedHookLoadPackage {
-    private static final String TAG = "qslock";
-
-    private static final int DISABLE2_NONE = 0;
-    private static final int DISABLE2_QUICK_SETTINGS = 1;
-    private static final int DISABLE2_NOTIFICATION_SHADE = 1 << 2;
-
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.android.systemui")) {
             return;
         }
 
-        XposedBridge.log(TAG + ": started");
+        XposedBridge.log("qslock" + ": started");
 
         XposedHelpers.findAndHookMethod("com.android.keyguard.KeyguardDisplayManager", lpparam.classLoader, "show", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Log.i(TAG, "show");
+                Log.i("qslock", "show");
                 Context context = (Context) AndroidAppHelper.currentApplication();
                 if (context == null) {
-                    Log.e(TAG, "context is null");
+                    Log.e("qslock", "context is null");
                     return;
                 }
                 Object statusBarManager = context.getSystemService("statusbar");
                 if (statusBarManager == null) {
-                    Log.e(TAG, "statusBarManager is null");
+                    Log.e("qslock", "statusBarManager is null");
                     return;
                 }
-                XposedHelpers.callMethod(statusBarManager, "disable2", new Class<?>[]{Integer.class}, DISABLE2_QUICK_SETTINGS | DISABLE2_NOTIFICATION_SHADE);
+                XposedHelpers.callMethod(statusBarManager, "disable2", new Class<?>[]{Integer.class}, -1);
             }
         });
 
         XposedHelpers.findAndHookMethod("com.android.keyguard.KeyguardDisplayManager", lpparam.classLoader, "hide", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Log.i(TAG, "hide");
+                Log.i("qslock", "hide");
                 Context context = (Context) AndroidAppHelper.currentApplication();
                 if (context == null) {
-                    Log.e(TAG, "context is null");
+                    Log.e("qslock", "context is null");
                     return;
                 }
                 Object statusBarManager = context.getSystemService("statusbar");
                 if (statusBarManager == null) {
-                    Log.e(TAG, "statusBarManager is null");
+                    Log.e("qslock", "statusBarManager is null");
                     return;
                 }
-                XposedHelpers.callMethod(statusBarManager, "disable2", new Class<?>[]{Integer.class}, DISABLE2_NONE);
+                XposedHelpers.callMethod(statusBarManager, "disable2", new Class<?>[]{Integer.class}, 0);
             }
         });
     }
